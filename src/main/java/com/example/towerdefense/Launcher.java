db47eae8b24d07b985f675a6be2e19c0762d2c9a
alessia.toso <alessia.toso@itiszuccante.edu.it>
2026-04-21 14:29:53 +0200

    .

--- src/main/java/com/example/towerdefense/Launcher.java
+++ src/main/java/com/example/towerdefense/Launcher.java
@@ -5,5 +5,9 @@ import javafx.application.Application;
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
+
+
+
+
     }
 }
