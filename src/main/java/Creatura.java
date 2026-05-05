2bfdedb2d8c55ccee59d7c36419820f5bd50df37
alessia.toso <alessia.toso@itiszuccante.edu.it>
2026-05-05 14:06:07 +0200

    aggiungo classe creatura

--- src/main/java/Creatura.java
+++ src/main/java/Creatura.java
@@ -1,2 +1,16 @@
+import javafx.scene.image.Image;
+
 public class Creatura {
+    protected double x, y, velocita;
+    protected double purificazione = 0;
+    protected int frameAttuale;
+    protected Image spriteSheet;
+
+    public Creatura(int x, int y, Image img){
+        this.x = x;
+        this.y = y;
+        this.spriteSheet = img;
+    }
+
+
 }
