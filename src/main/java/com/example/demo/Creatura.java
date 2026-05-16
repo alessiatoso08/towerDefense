9b94a31bab069e49051f4a8437669360d5bc509c
aless <alessia.toswo@itiszuccante..edu.it>
2026-05-16 16:00:25 +0200

    prova

--- src/main/java/com/example/demo/Creatura.java
+++ src/main/java/com/example/demo/Creatura.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
-public abstract class Creatura { //prova 2
+public abstract class Creatura {
     protected double x, y, velocita;
     protected double purificazione = 0;
     protected int frameAttuale = 0;
